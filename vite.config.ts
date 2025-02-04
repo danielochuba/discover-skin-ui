c46dee3bde4bcf65b3b0d1302c04af4235f4b8e2
Daniel Ochuba Ugochukwu <danielochuba78@gmail.com>
2025-02-04 05:16:19 +0100

    configure path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import * as path from "path";
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -8,4 +9,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+    },
+    extensions: [".mjs", ".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
 })
